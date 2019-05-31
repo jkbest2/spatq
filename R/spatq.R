eab6050199d4035fd15b3865fe3a325cadfb3d12
John Best <jkbest@gmail.com>
2019-05-31 12:30:01 -0700

    Add TMB and RcppEigen deps, initial package docs

---
+++ R/spatq.R
@@ -0,0 +1,12 @@
+#' Spatially varying catchability for fishery-dependent observations
+#'
+#' A package for fitting index standardization models that combine
+#' fishery-independent and fishery-dependent observations, where the
+#' fishery-dependent observations are allowed a spatially varying
+#' catchability.
+#'
+#' @docType package
+#' @name spatq
+#' @useDynLib spatq
+NULL
+
