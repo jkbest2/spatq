0582f345203acd1df5aaf914f8042256b8162e79
John Best <jkbest@gmail.com>
2019-11-20 14:36:51 -0800

    Add fitting and reporting functions with documentation

--- R/fit_spatq.R
+++ R/fit_spatq.R
@@ -4,8 +4,19 @@
 ##' @param obj A spatq \code{ADFun}, as returned by \code{prepare_adfun} or
 ##'   \code{make_sim_adfun}
 ##' @param ... Additional options to pass to \code{optim}, e.g. \code{control}
-##' @return An optimiz
+##' @return An optimization object, report list, or sdreport list
 ##' @author John Best
+##' @export
 fit_spatq <- function(obj, ...) {
-  optim(obj$par, obj$fn, obj$gr, ...)
+  stats::optim(obj$par, obj$fn, obj$gr, ...)
+}
+
+##' @describeIn fit_spatq Get object report
+report_spatq <- function(obj) {
+  obj$report()
+}
+
+##' @describeIn fit_spatq Get object sdreport
+sdreport_spatq <- function(obj) {
+  sdreport(obj)
 }
