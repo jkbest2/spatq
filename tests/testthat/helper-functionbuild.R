a69b70f23e00c5aade8eb1663149b4c1626a262c
John Best <jkbest@gmail.com>
2019-11-22 14:55:34 -0800

    Specify BFGS as `optim` algorithm

--- tests/testthat/helper-functionbuild.R
+++ tests/testthat/helper-functionbuild.R
@@ -1,7 +1,10 @@
 obj_fbuild <- make_sim_adfun(repl = 1, sc = "simple",
-                             sub_df = data.frame(vessel_idx = c(1, 2),
-                                                 n = c(100, 200)),
+                             sub_df = data.frame(vessel_idx = 2,
+                                                 n = 1000),
                              root_dir = normalizePath(
                                system.file("testdata", package = "spatq")),
-                             max_T = 15)
+                             max_T = 5)
 
+## fit <- fit_spatq(obj_fbuild)
+## rep <- report_spatq(obj_fbuild)
+## sdr <- sdreport_spatq(obj_fbuild)
