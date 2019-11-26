1fa82ed85f485fcbe5754e08d5619682ae2e5390
John Best <jkbest@gmail.com>
2019-11-26 15:44:52 -0800

    Squashed commit to add functions for evaluating model (#18)
    * Squashed commit to add functions for evaluating model
    
    commit 070107f36b84a82f43f173fea7cd1c5822a43774
    Author: John Best <jkbest@gmail.com>
    Date:   Tue Nov 26 15:26:47 2019 -0800
    
        Bump to v0.2.1
    
    commit f97047d182d17eb4bd4cee1b956eb2a96e3c19cb
    Author: John Best <jkbest@gmail.com>
    Date:   Tue Nov 26 15:25:43 2019 -0800
    
        Add percentile function for calibration
    
    commit 2159a16858b186b3ba1cb1b085b82ee03b15aac9
    Author: John Best <jkbest@gmail.com>
    Date:   Tue Nov 26 15:19:00 2019 -0800
    
        Add RMSE function
    
    commit 3b9b10f70953f344b46f787cd86ffc615813afca
    Author: John Best <jkbest@gmail.com>
    Date:   Tue Nov 26 10:57:51 2019 -0800
    
        Correct spelling in `make_sim_adfun` documentation
    
    commit eb65b9fd72caa746201dcdcdf8af2139e8019c1e
    Author: John Best <jkbest@gmail.com>
    Date:   Tue Nov 26 10:54:15 2019 -0800
    
        Add bias metric function
    
    commit 303f23375d29071616be3530dab416f9646b6eba
    Author: John Best <jkbest@gmail.com>
    Date:   Tue Nov 26 10:33:21 2019 -0800
    
        Add index-rescaling function
    
    * Fix R CMD check warnings

---
+++ R/eval_sims.R
@@ -0,0 +1,70 @@
+##' Estimate the relationship between estimated indices and true biomass for
+##' simulated data sets. Uses linear regression to estimate their relationship,
+##' where one indicates unbiasedness. Requires a data frame with columns
+##' 'est_index', 'true_biomass', and 'repl'.
+##'
+##' @title Estimate bias of estimation models
+##' @param fit_df Data frame with estimated indices, true biomass, and replicate
+##'   number
+##' @return A list with 'alpha', the offsets for each replicate; 'delta', the
+##'   bias coefficient; 'epsilon', the residuals; and 'sigma', the standard
+##'   deviation of the regression.
+##' @author John Best
+##' @export
+bias_metric <- function(fit_df) {
+  if (!all(c("est_index", "true_biomass", "repl") %in% names(fit_df)))
+    stop("Data frame must have columns \'est_index\', \'true_biomass\', and \'repl\'")
+  mod <- stats::lm(log(est_index) ~ 0 + factor(repl) + log(true_biomass),
+            data = fit_df)
+  mod_coef <- stats::coef(mod)
+  list(alpha = mod_coef[!grepl("true_biomass", names(mod_coef))],
+       delta = mod_coef[grepl("true_biomass", names(mod_coef))],
+       epsilon = stats::resid(mod),
+       sigma = stats::sigma(mod))
+
+}
+
+##' Rescale indices of abundance so that they have product one (mean-log zero).
+##' This facilitates comparison among models that only estimate relative
+##' abundance and with simulated true abundances
+##'
+##' @title Rescale indices
+##' @param b Unscaled indices/biomass
+##' @return A vector of indices scaled to have a product 1 (mean log of zero)
+##' @author John Best
+##' @export
+rescale_index <- function(b) {
+  scale <- exp(mean(log(b)))
+  i <- b / i
+  attr(i, "scale") <- scale
+  i
+}
+
+##' Calculate the root mean square error of the estimated indices of abundance
+##' compared to the true, simulated values. Expects that these will be on a
+##' common scale, e.g. by using \code{rescale_indices}.
+##'
+##' @title Root mean square error
+##' @param est_index Estimated rescaled index
+##' @param true_index True rescaled index
+##' @return Root mean square error of the estimates
+##' @author John Best
+##' @export
+rmse_metric <- function(est_index, true_index) {
+  sqrt(mean((est_index - true_index)^2))
+}
+
+##' In order to assess calibration of the estimation models, find the percentile
+##' of the true index under the estimated distribution, assumed to be normal
+##' with estimated mean and standard deviation.
+##'
+##' @title Percentile of true value in estimated distribution
+##' @param true_index True rescaled index
+##' @param est_index Estimated index of abundance
+##' @param est_sd Estimated standard deviation of index
+##' @return The percentile of the true value
+##' @author John Best
+##' @export
+true_percentile <- function(true_index, est_index, est_sd) {
+  stats::pnorm(true_index, est_index, est_sd)
+}
