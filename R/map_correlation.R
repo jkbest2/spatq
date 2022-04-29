37198cd919365cb9086fc1a466fd1f22c5d8519d
John Best <jkbest@gmail.com>
2022-04-29 11:16:48 -0700

    Allow choice of correlation metric

--- R/map_correlation.R
+++ R/map_correlation.R
@@ -9,20 +9,21 @@
 ##' @param est Array of estimated biomass, with matching first and second
 ##'   dimensions
 ##' @param byyear Calculate correlations by year?
+##' @param method Which correlation coefficient to use; passed to
+##'   \code{\link[stats]{cor}}
 ##' @return Correlation, or vector of correlations by year
 ##' @author John K Best
 ##' @export
-map_correlation <- function(true, est, byyear = FALSE){
+map_correlation <- function(true, est, byyear = FALSE, method = "pearson") {
   all(dim(true)[1:2] == dim(est)[1:2]) || error("Domains must be the same size")
   nyears <- min(dim(true)[3], dim(est)[3])
   if (byyear) {
     mapcor <- vapply(1:nyears,
                      function(yr)
-                       cor(c(true[, , yr]), c(est[, , yr])),
+                       cor(c(true[, , yr]), c(est[, , yr]), method = method),
                      1.0)
   } else {
-    mapcor <- cor(true[, , 1:nyears], est[, , 1:nyears])
+    mapcor <- cor(true[, , 1:nyears], est[, , 1:nyears], method = method)
   }
   return(mapcor)
 }
-
